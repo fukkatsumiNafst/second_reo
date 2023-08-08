1b20f4e7f8c840c38c8f73f861928b0a9da09222
fukkatsumiNafst <136838031+fukkatsumiNafst@users.noreply.github.com>
2023-08-08 21:22:37 +0700

    Add files via upload

--- models.py
+++ models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
+import datetime
 
 class Adv(models.Model):
     title = models.CharField('заголовок', max_length=128)
@@ -7,3 +11,19 @@ class Adv(models.Model):
     auction = models.BooleanField('торг', help_text='отметьте если торг уместен')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color:green; font-weight: bold"></span>',
+                created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y at %H:%M:%S')
+
+    class Meta:
+        db_table = 'advertisements'
+
+    def __str__(self) -> str:
+        return f"Advertisements(id = {self.id}, title = {self.title}, price = {self.price})"
