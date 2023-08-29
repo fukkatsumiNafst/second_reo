db4595bed0ed56be9230e85e22cf717cb64d7d85
fukkatsumiNafst <136838031+fukkatsumiNafst@users.noreply.github.com>
2023-08-29 21:57:43 +0700

    Add files via upload

--- views.py
+++ views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Adv
 
 def index(requset):
-    return HttpResponse('домашка к 4 уроку')
+    adv = Adv.object.all()
+    context = {'adv': adv}
+    return render(request, 'index.html', context)
+
+
+def top_sellers(requset):
+    return render(request, 'top-sellers.html')
